20c720efd2e4b9ca5338578923ee00448032c138
yoni <yonatanglozman@gmail.com>
2018-09-11 13:10:46 +0300

    massive change

---
+++ Web1/Controllers/AppointmentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web1.Models;
+
+namespace Web1.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        private WebContext db = new WebContext();
+
+        // GET: Appointments
+        public ActionResult Index()
+        {
+            return View(db.Appointments.ToList());
+        }
+
+        // GET: Appointments/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Appointments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Date_Time,IsAvaliable")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(appointment);
+        }
+
+        // GET: Appointments/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Date_Time,IsAvaliable")] Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appointment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointments/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web1/DAL/DBInit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web1.Models;
+
+namespace Web1.DAL
+{
+    public class DBInit : System.Data.Entity.DropCreateDatabaseIfModelChanges<WebContext>
+    {
+        protected override void Seed(WebContext context)
+        {
+            //Initialize doctors
+            var doctors = new List<Doctor>
+            {
+                new Doctor{ID="123456123", FirstName="Amir", LastName="Mizrahi", Password="123456", Specialization="Eyes"},
+                new Doctor{ID="987654321", FirstName="Inon", LastName="Weber", Password="123456", Specialization="Family"}
+            };
+            doctors.ForEach(d => context.Doctors.Add(d));
+            context.SaveChanges();
+
+            //Initialize patients
+            var patients = new List<Patient>
+            {
+                new Patient{ID="444444123", FirstName="Yosef", LastName="Yaakov", Password="123456", Address="Hatikva 6", City="Ramla", Age=23, Latitude=31.92923, Longitude=34.8823322, Zip="43254"},
+                new Patient{ID="123456789", FirstName="Yonatan", LastName="Glozman", Password="123456", Address="Lean 2", City="Ramat Gan", Age=22, Latitude=32.0837731, Longitude=34.810031, Zip="12345"}
+            };
+            patients.ForEach(p => context.Patients.Add(p));
+            context.SaveChanges();
+        }
+    }
+}
