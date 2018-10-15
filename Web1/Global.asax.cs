6874ee1ffb10855b073a82341039d8a1aadb2fda
amirmizrahi <amirmizrahi@gmail.com>
2018-10-15 21:34:08 +0300

    si

--- Web1/Global.asax.cs
+++ Web1/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Web1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //nig
         }
     }
 }
